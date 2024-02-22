bd970dcbfed03325913a856d80d618800632ef07
nasimabo <nasimuddin555k@gmail.com>
2024-02-22 12:47:25 +0600

    ternary_operator

---
+++ ternary_operator.py
@@ -0,0 +1,20 @@
+# Ternary operator if
+'''food = input("food name : ")
+eat = "yes" if food == "cake" else "No"
+print(eat)'''
+
+
+'''food = input("food name : ")
+
+print("sweet") if food == "cake" or food == "jelabi" else print("not sweet")'''
+
+
+# clever ternary operator if
+
+'''age = int(input("enter your age: "))
+vot = ("no","yes")[age >= 18 ]
+print(vot)'''
+
+sal = float(input("enter the sal: "))
+tax = sal*(.1,.2) [sal >= 50000]
+print(tax)
